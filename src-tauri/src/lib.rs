026804b787143768081e0a4963d0bd68e01ae49a
Dio <dio1chou@gmail.com>
2025-07-29 15:43:24 +0800

    feat: 添加日志插件并配置日志级别

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,7 +7,12 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_log::Builder::new().build())
+        .plugin(
+            tauri_plugin_log::Builder::new()
+                .level(log::LevelFilter::Info) // 你可以设置一个全局的默认级别，例如 Info
+                .level_for("tao", log::LevelFilter::Warn) // 将 tao crate 的日志级别设为 Warn
+                .build()
+        )
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_store::Builder::new().build())
