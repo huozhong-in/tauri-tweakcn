2de5f3be0be1ccd3bcbf4371c8ba7ceb483388d4
Dio <dio1chou@gmail.com>
2025-08-02 03:37:45 +0800

    feat: 优化PDF文档转换功能，增强图片处理和导出选项，添加JSON保存功能

--- api/test_docling_01.py
+++ api/test_docling_01.py
@@ -17,12 +17,9 @@ from docling_core.types.doc import (
 from docling.datamodel.document import (
     ConversionResult,
 )
-# from docling.utils.export import generate_multimodal_pages
-# from docling.utils.utils import create_hash
 import logging
 import time
 from pathlib import Path
-# from random import randint
 
 _log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -37,6 +34,7 @@ def main():
     # with the image field
     pipeline_options = PdfPipelineOptions()
     pipeline_options.generate_picture_images = True
+    pipeline_options.generate_table_images = True
     # pipeline_options.generate_page_images = True
     pipeline_options.images_scale = IMAGE_RESOLUTION_SCALE
     pipeline_options.do_picture_description = True
@@ -45,6 +43,7 @@ def main():
         url="http://localhost:1234/v1/chat/completions",
         params=dict(
             model="google/gemma-3-4b",
+            # model="mlx-community/qwen2.5-vl-7b-instruct",
             seed=42,  # for reproducibility
             temperature=0.2,
             max_completion_tokens=250,
@@ -66,8 +65,9 @@ def main():
     result: ConversionResult = converter.convert(
         # source="/Users/dio/Downloads/Context Engineering for AI Agents_ Lessons from Building Manus.pdf"
         source="/Users/dio/Downloads/AI代理的上下文工程：构建Manus的经验教训.pdf"
+        # source=Path("layout-parser-paper.pdf"),
     )
-    doc = result.document
+    doc: DoclingDocument = result.document
     print(f"Document has {len(doc.pages)} pages.")
 
     output_dir = Path("test_docling_01")
@@ -75,18 +75,31 @@ def main():
         output_dir.mkdir(parents=True, exist_ok=True)
     doc_filename = result.input.file.stem
 
-    # dump_json_path = Path("./test_docling_01.json")
-    # dump_json_path.write_text(result.document.model_dump_json(indent=2))
-    # print(result.document.pictures[2].get_annotations()[0].text)
+    print(len(result.document.pictures), "pictures found in the document.")
+    picture_3 = result.document.pictures[2]
+    print(picture_3.prov[0].page_no, "is the page number of picture 3.")
+    print(picture_3.image.model_dump_json(indent=2, exclude=["uri"]))
+    '''
+    "image": {
+        "mimetype": "image/png",
+        "dpi": 144,
+        "size": {
+          "width": 853.0,
+          "height": 414.0
+        },
+        "uri": "data:image/png;base64,..."
+    }
+    '''
+    print(picture_3.get_annotations()[0].text)
+    dump_json_path = output_dir / "test_docling_01.json"
+    result.document.save_as_json(
+        filename=dump_json_path,
+        indent=2,
+        image_mode=ImageRefMode.REFERENCED, # "uri": "test_docling_01/image_000003_ac172f3.png"
+        artifacts_dir=Path(".")
+    )
 
-    # export to markdown
-    # markdown_path = Path("./test_docling_01.md")
-    # markdown_path.write_text(doc.export_to_markdown(
-    #     image_mode=ImageRefMode.PLACEHOLDER,
-    #     image_placeholder="![{page_number}]{image_url}",
-    # ))
-
-    # Save page images
+    # Save page images，也就是把整页存为一张图片
     # for page_no, page in result.document.pages.items():
     #     page_no = page.page_no
     #     page_image_filename = output_dir / f"{doc_filename}-{page_no}.png"
@@ -96,7 +109,7 @@ def main():
     # Save images of figures and tables
     table_counter = 0
     picture_counter = 0
-    for element, _level in result.document.iterate_items():
+    for element, _ in result.document.iterate_items():
         if isinstance(element, TableItem):
             table_counter += 1
             element_image_filename = (
@@ -113,6 +126,15 @@ def main():
             with element_image_filename.open("wb") as fp:
                 element.get_image(result.document).save(fp, "PNG")
 
+    # Export Document Tags format:
+    # result.document.save_as_doctags(
+    #     filename=output_dir / f"{doc_filename}.doctags",
+    # )
+    
+    # Save markdown with embedded pictures
+    md_filename = output_dir / f"{doc_filename}-with-images-base64.md"
+    result.document.save_as_markdown(md_filename, image_mode=ImageRefMode.EMBEDDED)
+
     # Save markdown with externally referenced pictures
     md_filename = output_dir / f"{doc_filename}-with-image-refs.md"
     result.document.save_as_markdown(
@@ -120,23 +142,17 @@ def main():
         image_mode=ImageRefMode.REFERENCED,
         artifacts_dir=Path(".")
     )
-
-    # Export Document Tags format:
-    # result.document.save_as_doctags(
-    #     filename=output_dir / f"{doc_filename}.doctags",
-    # )
-    
-    # Save markdown with embedded pictures
-    md_filename = output_dir / f"{doc_filename}-with-images.md"
-    result.document.save_as_markdown(md_filename, image_mode=ImageRefMode.EMBEDDED)
-
-    # Save markdown with externally referenced pictures
-    md_filename = output_dir / f"{doc_filename}-with-image-refs.md"
-    result.document.save_as_markdown(md_filename, image_mode=ImageRefMode.REFERENCED)
+    # OR: export to markdown
+    markdown_path = output_dir / f"{doc_filename}-without-annotations.md"
+    markdown_path.write_text(doc.export_to_markdown(
+        image_mode=ImageRefMode.PLACEHOLDER,
+        image_placeholder="<!-- image -->",
+        include_annotations=False,
+    ))
 
     # Save HTML with externally referenced pictures
-    html_filename = output_dir / f"{doc_filename}-with-image-refs.html"
-    result.document.save_as_html(html_filename, image_mode=ImageRefMode.REFERENCED)
+    # html_filename = output_dir / f"{doc_filename}-with-image-refs.html"
+    # result.document.save_as_html(html_filename, image_mode=ImageRefMode.REFERENCED)
 
     end_time = time.time() - start_time
 
